c9492c29356736671826014ea6e19f38d0add03b
DriedSponge <jortuck0810@gmail.com>
2020-04-29 01:30:37 -0700

    Changed some stuff

--- views/privacyterms.php
+++ views/privacyterms.php
@@ -12,7 +12,7 @@
         <div class="container-fluid-lg">
             <div class="container-fluid">
                 <div class="content-box">
-                    <h1 class="text-left">Our Privacy Policy</h1>
+                    <h1 class="text-left">Privacy Policy & Terms Of Service</h1>
                     <h2 class="text-left">The Information We Collect and/or Receive</h2>
                     <p><strong>Analytics</strong></p>
                     <p>We use Google Analytics to evaluate your use of the Services, compile reports on activity, collect demographic data, analyze performance metrics, and collect and evaluate other information relating to the Services and mobile and Internet usage. This third party uses cookies and other technologies to help analyze and provide us the data. By accessing and using the Services, you consent to the processing of data about you by Google Analytics in the manner and for the purposes set out in this Privacy Policy.</p>
@@ -21,7 +21,7 @@
                     <p>We collect and retain your email address when you register for an account in case we need to contact you. We may also use your email address for sending you updates about your account.</p>
 
                     <h2 class="text-left">How We Protect Your Information</h2>
-                    <p>We take commercially reasonable steps to protect the Information from loss, misuse, and unauthorized access, disclosure, alteration, or destruction. Please understand, however, that no security system is impenetrable. We follow generally accepted industry standards to protect the Information submitted to us, both during transmission and once we receive it. However, we cannot guarantee the security of our databases, nor can we guarantee that the information you supply will not be intercepted while being transmitted to and from us over the Internet.</p>
+                    <p>We take commercially reasonable steps to protect the Information from loss, misuse, and unauthorized access, disclosure, alteration, or destruction. Please understand, however, that no security system is impenetrable. We follow generally accepted industry standards to protect the information submitted to us, both during transmission and once we receive it. However, we cannot guarantee the security of our databases, nor can we guarantee that the information you supply will not be intercepted while being transmitted to and from us over the Internet.</p>
 
                     <h2 class="text-left">Data Retention & Deletion</h2>
                     <p>We will retain your information for as long as your account is active or as needed to provide you services. If you wish to cancel your account or request that we no longer use your information to provide you services, go to your account settings and delete your account. We do retain some, non-sensitive, information of banned users to prevent them from returning.</p>
